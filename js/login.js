bdf50b9a6a747b33ed8f1ade1307088a9fd953bf
Abdullah <56775516+Zaazou@users.noreply.github.com>
2023-01-19 03:33:14 +0200

    login validation almost done

--- js/login.js
+++ js/login.js
@@ -3,15 +3,26 @@ let password = document.getElementById("password");
 
 
 
+function checkUserInput() {
+    let vaildUserName = username.checkValidity();
+    let vaildPassword = password.checkValidity();
+    if (vaildUserName && vaildPassword) {
+        CheckUsernameAndPassword();
+    }
+
+}
+
 async function CheckUsernameAndPassword() {
     let usernameValue = username.value;
     let passwordValue = password.value;
     let empData = await fetch(`http://localhost:3000/employees?username=${usernameValue}&password=${passwordValue}`);
     let empDataObject = await empData.json();
+    console.log(empDataObject);
     if (empDataObject[0].role == "admin") {
         alert("this is Admin ")
     } else if (empDataObject[0].role == "employee") {
         alert("this is employee")
+        // location.href("")
 
     }
     else {
@@ -22,11 +33,7 @@ async function CheckUsernameAndPassword() {
 let loginBtn = document.querySelector('button[type="submit"]')
 
 loginBtn.addEventListener('click', (e) => {
+
     e.preventDefault();
-    if (CheckUsernameAndPassword()) {
-
-    }
-
-
-
+    checkUserInput();
 })
