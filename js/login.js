8542f7339e2c4db8a237e9871bda25c122c76568
Abdullah <56775516+Zaazou@users.noreply.github.com>
2023-01-19 04:21:13 +0200

    login is done

--- js/login.js
+++ js/login.js
@@ -1,24 +1,15 @@
 let username = document.getElementById("username");
 let password = document.getElementById("password");
 
-
-
-function checkUserInput() {
-    let vaildUserName = username.checkValidity();
-    let vaildPassword = password.checkValidity();
-    if (vaildUserName && vaildPassword) {
-        CheckUsernameAndPassword();
-    }
-
-}
-
 async function CheckUsernameAndPassword() {
     let usernameValue = username.value;
     let passwordValue = password.value;
     let empData = await fetch(`http://localhost:3000/employees?username=${usernameValue}&password=${passwordValue}`);
     let empDataObject = await empData.json();
-    console.log(empDataObject);
-    if (empDataObject[0].role == "admin") {
+    if (empDataObject.length == 0) {
+        alert("Please Check your username and password and try again")
+    }
+    else if (empDataObject[0].role == "admin") {
         alert("this is Admin ")
     } else if (empDataObject[0].role == "employee") {
         alert("this is employee")
@@ -30,10 +21,13 @@ async function CheckUsernameAndPassword() {
     }
 }
 
-let loginBtn = document.querySelector('button[type="submit"]')
+let form = document.querySelector('form')
 
-loginBtn.addEventListener('click', (e) => {
-
+form.addEventListener('submit', (e) => {
     e.preventDefault();
-    checkUserInput();
+    let vaildUserName = username.checkValidity();
+    let vaildPassword = password.checkValidity();
+    if (vaildUserName && vaildPassword) {
+        CheckUsernameAndPassword();
+    }
 })
