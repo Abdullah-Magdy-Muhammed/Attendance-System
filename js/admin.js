d0be8cba810b7dbdf5d496577154e8e6f286ae37
Abdullah <56775516+Zaazou@users.noreply.github.com>
2023-01-23 18:16:44 +0200

    admin dashboard is done except monthly report

--- js/admin.js
+++ js/admin.js
@@ -1,19 +1,61 @@
-// function to controll flow of admin panel 
-// function changeDisplayedTable()
+// function for navbar to be dynamic 
 
-let allemp;
-let allAttendence;
-let navBar = document.getElementById("reportsTabs")
+// all divs which have tables of reports
+let pendingReport = document.getElementById("pending");
+let fullReport = document.getElementById("fullreport");
+let monthlyReport = document.getElementById("monthlyreport");
+let dailyReport = document.getElementById("dailyreport");
 
-navBar.addEventListener("click", (e) => {
-    // this.classList.remove("active");
-    console.log(navBar)
+// all navbar buutons
+let pendingBtn = document.getElementById("candidates");
+let allEmployeesBtn = document.getElementById("fullreportbtn");
+let dailyReportBtn = document.getElementById("dailyreportbtn");
+let monthlyReportBtn = document.getElementById("monthlyreportbtn")
 
-    // e.classList.add("active")
+allEmployeesBtn.addEventListener("click", (e) => {
+    pendingBtn.classList.remove("active");
+    dailyReportBtn.classList.remove("active");
+    monthlyReportBtn.classList.remove("active");
+    pendingReport.classList.remove("active");
+    document.getElementById("dailyreport").classList.remove("active");
+    monthlyReport.classList.remove("active");
+    e.target.classList.add("active");
+    fullReport.classList.add("active");
+})
+dailyReportBtn.addEventListener("click", (e) => {
+    pendingBtn.classList.remove("active");
+    monthlyReportBtn.classList.remove("active");
+    allEmployeesBtn.classList.remove("active");
+    pendingReport.classList.remove("active");
+    monthlyReport.classList.remove("active");
+    fullReport.classList.remove("active");
+    e.target.classList.add("active");
+    document.getElementById("dailyreport").classList.add("active")
+
+})
+
+monthlyReportBtn.addEventListener("click", (e) => {
+    pendingBtn.classList.remove("active");
+    dailyReportBtn.classList.remove("active");
+    allEmployeesBtn.classList.remove("active");
+    pendingReport.classList.remove("active");
+    document.getElementById("dailyreport").classList.remove("active");
+    fullReport.classList.remove("active");
+    e.target.classList.add("active");
+    monthlyReport.classList.add("active");
+})
+pendingBtn.addEventListener("click", (e) => {
+    monthlyReportBtn.classList.remove("active");
+    allEmployeesBtn.classList.remove("active");
+    dailyReportBtn.classList.remove("active");
+    monthlyReport.classList.remove("active");
+    fullReport.classList.remove("active");
+    document.getElementById("dailyreport").classList.remove("active");
+    e.target.classList.add("active");
+    pendingReport.classList.add("active");
 })
 
 // first table in admin page (pending people who need admin permission)
-
 pendingBody = document.getElementById("allpending");
 fetch("http://localhost:3000/employees?accepted=false", {
     method: "GET",
@@ -45,12 +87,46 @@ fetch("http://localhost:3000/employees?accepted=false", {
                 postition.innerText = "Admin"
             }
             tr.appendChild(postition)
-
             let confirm = document.createElement("td");
-            confirm.innerHTML = "<button id=`accept-${i}` class='btn-light px-2'>&check;</button> <button class='btn-danger px-2'>X</button>"
-            tr.appendChild(confirm)
+            confirm.innerHTML = `<button id='accept-${data[i].id}' class='btn-light px-2'>&check;</button> <button id= 'delete-${data[i].id}' class='btn-danger px-2'>X</button>`
+            tr.appendChild(confirm);
+
+            // confirm and delete
+            let deleteBtn = document.getElementById(`delete-${data[i].id}`)
+            deleteBtn.addEventListener("click", (e) => {
+                let row = e.target.parentElement.parentElement;
+                if (window.confirm("Are You Sure to delete this row")) {
+
+                    fetch(`http://localhost:3000/employees/${data[i].id}`, {
+                        method: "DELETE",
+                        headers: { "Content-type": "application/JSON;charset=UTF-8" },
+
+                    })
+                        .then((response) => response.json())
+                        .then((data) => {
+                            row.remove()
+                        })
+                }
+            })
+            let confirmBbtn = document.getElementById(`accept-${data[i].id}`)
+            confirmBbtn.addEventListener("click", (e) => {
+                let row = e.target.parentElement.parentElement;
+                if (window.confirm("Are You Sure to accept this employee")) {
+
+                    fetch(`http://localhost:3000/employees/${data[i].id}`, {
+                        method: "PATCH",
+                        headers: { "Content-type": "application/JSON;charset=UTF-8" },
+                        body: JSON.stringify({ accepted: true })
+                    })
+                        .then((response) => response.json())
+                        .then((data) => {
+                            row.remove();
+                        })
+                }
+            })
         }
     })
+
 // full employee report (Second table in admin page)
 fullEmployeeReport = document.getElementById("fullEmployeeReports");
 
@@ -59,8 +135,6 @@ fetch("http://localhost:3000/employees", {
 })
     .then((response) => response.json())
     .then((data) => {
-
-
         for (let i = 0; i < data.length; i++) {
             let tr = document.createElement("tr");
             fullEmployeeReport.appendChild(tr);
@@ -89,16 +163,12 @@ fetch("http://localhost:3000/employees", {
             let startDate = document.createElement("td");
             startDate.innerText = data[i].startDate;
             tr.appendChild(startDate);
-
         }
     })
 
 // Daily Report
 dailyReport = document.getElementById("dailyReport");
-
 const empUsername = document.getElementById("username");
-
-
 fetch(`http://localhost:3000/employees`, {
     method: "GET",
     headers: { "Content-type": "application/JSON;charset=UTF-8" },
@@ -106,7 +176,6 @@ fetch(`http://localhost:3000/employees`, {
     .then((response) => response.json())
     .then((data) => {
         allemp = data;
-        console.log(allemp);
         userId = allemp[0].id
         let date = new Date();
         let today = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
@@ -117,8 +186,6 @@ fetch(`http://localhost:3000/employees`, {
             .then((response) => response.json())
             .then((data) => {
                 allAttendence = data;
-                console.log(allAttendence)
-                debugger
                 allemp.forEach((emp) => {
                     let tr = document.createElement("tr");
                     for (let i = 0; i < allAttendence.length; i++) {
