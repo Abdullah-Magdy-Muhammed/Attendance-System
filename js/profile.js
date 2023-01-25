9315f0551c82bfc6b8505d08cdeab98517ef8a27
Abdullah <56775516+Zaazou@users.noreply.github.com>
2023-01-25 05:51:39 +0200

    employee profile

--- js/profile.js
+++ js/profile.js
@@ -6,8 +6,10 @@ let headAddress = document.getElementById("headaddress");
 let address = document.getElementById("address");
 let headName = document.getElementById("headname");
 let email = document.getElementById("email");
-let age = document.getElementById("age")
+let age = document.getElementById("age");
+let id = document.getElementById("id");
 
+let body = document.getElementById("employeeData");
 fetch(`http://localhost:3000/employees?username=${username}`, {
     method: "GET",
 })
@@ -19,8 +21,42 @@ fetch(`http://localhost:3000/employees?username=${username}`, {
         position.innerText = `${data[0].role}`;
         address.innerText = `${data[0].address}`;
         headAddress.innerText = `${data[0].address}`;
-
         email.innerText = `${data[0].email}`;
-        age.innerText = `${data[0].age}`
+        age.innerText = `${data[0].age}`;
 
-    })
+        fetch(`http://localhost:3000/attendence?employeeId=${data[0].id}`, {
+            method: "GET",
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                data.forEach(element => {
+                    let tr = document.createElement("tr");
+
+                    let id = document.createElement("td");
+                    id.innerText = `${element.employeeId}`;
+                    tr.appendChild(id);
+
+                    let date = document.createElement("td");
+                    date.innerText = `${element.date}`;
+                    tr.appendChild(date);
+
+                    let late = document.createElement("td");
+                    late.innerText = `${element.late}`;
+                    tr.appendChild(late);
+
+                    let absent = document.createElement("td");
+                    absent.innerText = `${element.absent}`;
+                    tr.appendChild(absent);
+
+                    let In = document.createElement("td");
+                    In.innerText = `${element.in}`;
+                    tr.appendChild(In);
+
+                    let out = document.createElement("td");
+                    out.innerText = `${element.out}`;
+                    tr.appendChild(out);
+
+                    body.appendChild(tr);
+                });
+            });
+    });
